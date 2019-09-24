0c07f9dbfc847ee7d96b5ad086c2ad8b9daadaac
Philippine Berton <Phi.berton@hotmail.fr>
2019-09-24 22:10:10 +0200

    omhiuvjh

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -1,12 +1,10 @@
-{
-  const navbar = document.querySelector('.navbar-home');
-  if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-home-view');
-      } else {
-        navbar.classList.remove('navbar-home-view');
-      }
-    });
-  }
-};
+const navbar = document.querySelector('.navbar-home');
+if (navbar) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= window.innerHeight) {
+      navbar.classList.add('navbar-home-view');
+    } else {
+      navbar.classList.remove('navbar-home-view');
+    }
+  });
+}
