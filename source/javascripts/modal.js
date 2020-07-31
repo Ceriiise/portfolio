9f08c52ace2e1ea5c3ef0aafddb41c838c55dc1d
Philippine Berton <Phi.berton@hotmail.fr>
2020-07-31 15:09:38 +0200

    ES5

---
+++ source/javascripts/modal.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded", function(event) {
+  var buttons = document.querySelectorAll('#modal-trigger');
+  var modal = document.querySelectorAll('.display-modal');
+
+  buttons.forEach(function (button) {
+    button.addEventListener('click', function(event) {
+      var modalTrigger = button.dataset.trigger;
+      var modal = document.querySelector(`[data-modal="${modalTrigger}"]`);
+      var scroll = window.scrollY;
+      modal.style.width = "100vw";
+      modal.style.top = window.scrollY + "px";
+      document.body.classList.add("stop-scrolling");
+
+      modal.addEventListener('click', function(event) {
+        modal.style.width = "0";
+        document.body.classList.remove("stop-scrolling");
+      });
+    });
+  });
+});
